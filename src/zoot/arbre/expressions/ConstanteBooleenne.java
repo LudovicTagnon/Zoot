2376e42982c9965e104b9fb3a2b4a9d1f6e05a10
LudovicTagnon <ludovic.tgn@outlook.com>
2023-02-04 20:28:05 +0100

    Ajout de la fonction toMIPS et getType à la classe ConstanteBooleenne

--- src/zoot/arbre/expressions/ConstanteBooleenne.java
+++ src/zoot/arbre/expressions/ConstanteBooleenne.java
@@ -8,5 +8,17 @@ public class ConstanteBooleenne extends Constante{
         super(val, n);
         valeur = Boolean.parseBoolean(val);
     }
+    @Override
+    public String toMIPS() {
+        if(valeur){
+            return "li $v0, 1\n";
+        }else{
+            return "li $v0, 0\n";
+        }
+    }
 
+    @Override
+    public String getType() {
+        return "booleen";
+    }
 }
