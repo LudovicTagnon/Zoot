19646dd6ec10285ea53bd5fd81e1becf8cd8b814
Lucas <lucas.villaume8@etu.univ-lorraine.fr>
2023-02-24 16:51:59 +0100

    Modif du constructeur de EntreeFonction

--- src/zoot/arbre/declarations/EntreeFonction.java
+++ src/zoot/arbre/declarations/EntreeFonction.java
@@ -2,25 +2,14 @@ package zoot.arbre.declarations;
 
 public class EntreeFonction extends Entree{
     private final int nbParam;
-    private final String type;
 
-    public EntreeFonction() {
-        super(""); //nom = "
-        this.nbParam = 0;
-        this.type = null;
-    }
-
-    public EntreeFonction(String nom, int nbParam, String type) {
+    public EntreeFonction(String nom, int nbParam) {
         super(nom);
         this.nbParam = nbParam;
-        this.type = type;
     }
 
     public int getNbParam() {
         return this.nbParam;
     }
 
-    public String getType() {
-        return this.type;
-    }
 }
