1404f52217e9b0931d8525567b88efda1d471ac5
Lucas <lucas.villaume8@etu.univ-lorraine.fr>
2023-03-26 22:09:50 +0200

    Modif AppelFonc

--- src/zoot/arbre/declarations/LFCT.java
+++ src/zoot/arbre/declarations/LFCT.java
@@ -1,5 +1,6 @@
 package zoot.arbre.declarations;
 import zoot.arbre.Symbole;
+import zoot.arbre.expressions.Expression;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,11 +13,13 @@ public class LFCT {
     private static LFCT instance = new LFCT();
 
     private HashMap<Entree, Symbole> params;
+    private ArrayList<Expression> par_apl; //Paramètres d'une fonction lors de son appel
 
     private LFCT() {
         fonctions = new ArrayList<>();
         debut = true;
         params = new HashMap<>();
+        par_apl = new ArrayList<>();
     }
 
     public void addFonction(Fonction f) {
@@ -27,9 +30,19 @@ public class LFCT {
         params.put(entree, sym);
     }
 
+    public void stockerParamsApl(Expression exp) { //Stocke provisoirement les paramètres d'une fonction
+        par_apl.add(exp);
+    }
+
     public HashMap<Entree, Symbole> destockParams() { //Renvoie les paramètres d'une fonction
         HashMap<Entree, Symbole> p = new HashMap<>(params); //On crée une copie profonde de parametres
         params.clear(); //On vide la liste des paramètres pour la prochaine fonction
+        return p;
+    }
+
+    public ArrayList<Expression> destockParamsApl() { //Renvoie les paramètres d'un appel de fonction
+        ArrayList<Expression> p = new ArrayList<>(par_apl); //On crée une copie profonde de parametres
+        par_apl.clear(); //On vide la liste des paramètres pour la prochaine fonction
         return p;
     }
 
--- src/zoot/arbre/expressions/AppelFonc.java
+++ src/zoot/arbre/expressions/AppelFonc.java
@@ -3,16 +3,21 @@ package zoot.arbre.expressions;
 import zoot.arbre.Symbole;
 import zoot.arbre.TDS;
 import zoot.arbre.declarations.Entree;
+import zoot.arbre.declarations.LFCT;
+
+import java.util.ArrayList;
 
 public class AppelFonc extends Expression{
 
     //Similaire à Idf
     Entree e;
     Symbole s;
+    ArrayList<Expression> params; //Paramètres d'une fonction lors de son appel
 
     public AppelFonc(Entree entree, int n) {
         super(n);
         e = entree;
+        params = LFCT.getInstance().destockParamsApl(); //On récupère les paramètres de l'appel de fonction
     }
 
     @Override
@@ -26,8 +31,22 @@ public class AppelFonc extends Expression{
         mips += "sw $ra,0($sp)\n"; //Sauvegarde du registre $ra
         mips += "sw $s1,-4($sp)\n"; //Sauvegarde du registre $s1
         mips += "addi $sp,$sp,-8\n"; //Décalage du pointeur de pile
+
+        //Todo: verifier
+        if (params.size() > 0) {
+            int cpt = params.size(); //Compteur pour les paramètres
+            mips += "#Paramètres de l'appel de fonction\n";
+            for (Expression param : params) {
+                cpt--;
+                mips += param.toMIPS();
+                mips += "sw $v0,"+cpt+"($sp)\n"; //Sauvegarde du registre $v0
+                mips += "addi $sp,$sp,-4\n"; //Décalage du pointeur de pile
+            }
+            mips += "addi $sp,$sp,"+params.size()+"\n"; //Décalage du pointeur de pile
+        }
+
         mips += "\n#Saut vers une fonction";
-        mips += "\njal "+e.getNom()+"\n";
+        mips += "\njal "+e.getNom()+"_"+e.getNoLigne()+"\n";
         mips += "#Load registres\n";
         mips += "lw $s1,4($sp)\n"; //Restauration du registre $s1
         mips += "lw $ra,8($sp)\n"; //Restauration du registre $ra
