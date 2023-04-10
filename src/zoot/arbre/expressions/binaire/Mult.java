db7b9674ad2446e28beec2036420a3179c5c8d72
Lucas <lucas.villaume8@etu.univ-lorraine.fr>
2023-04-10 14:22:06 +0200

    ajout de multiplication

---
+++ src/zoot/arbre/expressions/binaire/Mult.java
@@ -0,0 +1,72 @@
+package zoot.arbre.expressions.binaire;
+import zoot.arbre.expressions.Expression;
+import zoot.exceptions.CollectExcept;
+import zoot.exceptions.TypeIncompatibleException;
+
+public class Mult extends Binaire{
+
+
+    public Mult(Expression eg, Expression ed, int n) {
+        super(eg, ed, n);
+    }
+
+    @Override
+    public String getNom() {
+        return expGauche.getNom() + " * " + expDroite.getNom();
+    }
+
+    @Override
+    public void verifier() {
+        expGauche.verifier();
+        expDroite.verifier();
+        try{
+            if (!expGauche.getType().equals("entier") || !expDroite.getType().equals("entier")) {
+                System.out.println("Erreur de type : impossible d'effectuer une addition entre " + expGauche.getType() + " et " + expDroite.getType() + " à la ligne " + noLigne + ".");
+                throw new TypeIncompatibleException(expGauche, expDroite, noLigne);
+            }
+        }catch (TypeIncompatibleException e){
+            CollectExcept.getInstance().addException(e);
+        }
+    }
+
+    @Override
+    public String toMIPS() {
+        String mips = expGauche.toMIPS();
+        mips += "#Empile \nsw $v0,($sp) \nadd $sp,$sp,-4\n";
+
+        if (!expDroite.isFonc()) {
+            if (expDroite.isConstante()) { //si exp est une constante
+                if (expDroite.getType().equals("booleen")) { //si exp est un booléen
+                    mips += "la "; //load
+                } else { //si exp est un entier
+                    mips += "li "; //load
+                }
+
+            } else {
+                mips += "lw "; //load word si exp est une variable
+            }
+
+            mips += "$v0, " + expDroite.toMIPS(); //store la valeur dans v0
+        } else { //si exp est une fonction
+            mips += expDroite.toMIPS();
+        }
+
+        mips += "#Dépile \nadd $sp,$sp,4\nlw $t8,($sp)\nmul $v0, $t8, $v0\nmflo $v0\n";
+        return mips;
+    }
+
+    @Override
+    public String getType() {
+        return "entier";
+    }
+
+    @Override
+    public boolean isConstante() {
+        return expGauche.isConstante();
+    }
+
+    @Override
+    public boolean isFonc() {
+        return expGauche.isFonc();
+    }
+}
